9ec587143edaac6af0f2e5e3ecf017f00a78990c
CarlitosJPYT <59419806+CarlitosJPYT@users.noreply.github.com>
2020-07-17 19:59:41 -0500

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let  Name = 5
+let user = 'Juan'
+let number = 'Esteban'
+
+prompt('Whats your name?')
+
+if (Name === 5) {
+     console.log(user)
+}
+
+setTimeout(() => {
+     alert(number)
+}, 5000);
